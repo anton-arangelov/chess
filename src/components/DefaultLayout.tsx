9c9cc50b55498364dcd90430bba18a8c223550e2
anton-arangelov <arangelov.a@abv.bg>
2023-01-18 14:15:46 +0200

    Fixed an issue

---
+++ src/components/DefaultLayout.tsx
@@ -0,0 +1,71 @@
+import classNames from 'classnames'
+import { useRouter } from 'next/router'
+import { BaseSyntheticEvent, ReactNode, useState } from 'react'
+import menu from '../assets/menu-image.jpg'
+
+export const DefaultLayout = ({ children }: { children: ReactNode }) => {
+  const { asPath, push } = useRouter()
+  const [shouldAppear, setShouldAppear] = useState(false)
+
+  const handleNavigation = (e: BaseSyntheticEvent) => {
+    if (
+      e.target.id !== 'menuIcon' &&
+      e.target.id !== 'gameMenu' &&
+      shouldAppear
+    ) {
+      setShouldAppear(false)
+    }
+  }
+
+  return (
+    <>
+      <div className="relative" onClick={handleNavigation}>
+        <button
+          id="menuIcon"
+          className={classNames('fixed left-3 z-40', {
+            'top-[60px]': asPath.includes('millionaire'),
+            'top-[8%]': asPath === '/',
+            'bottom-[60px]': asPath.includes('shoot-the-duck')
+          })}
+          onClick={() => setShouldAppear(true)}
+        >
+          <img
+            src={menu.src}
+            alt=""
+            className="w-10 sm:w-[60px] h-10 sm:h-[60px] rounded-[10px]"
+          />
+        </button>
+        <div
+          id="gameMenu"
+          className={classNames(
+            '-translate-y-[48px] w-full py-3 bg-yellow-100 rounded-b',
+            'flex justify-between items-center px-10 fixed z-40 transition duration-300',
+            {
+              'translate-y-0': shouldAppear
+            }
+          )}
+        >
+          <button
+            className="transition duration-300 hover:text-gray-700 hover:scale-125"
+            onClick={() => push('/')}
+          >
+            Chess
+          </button>
+          <button
+            className="transition duration-300 hover:text-gray-700 hover:scale-125"
+            onClick={() => push('/shoot-the-duck')}
+          >
+            Duck
+          </button>
+          <button
+            className="transition duration-300 hover:text-gray-700 hover:scale-125"
+            onClick={() => push('/millionaire')}
+          >
+            Millionaire
+          </button>
+        </div>
+        {children}
+      </div>
+    </>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,16 @@ import { ApolloProvider } from '@apollo/client'
 import { Provider } from 'react-redux'
 import client from '../../apollo-client'
 import { wrapper } from '../store/store'
+import { DefaultLayout } from '../components/DefaultLayout'
 
 function App({ Component, pageProps }: AppProps) {
   const { store } = wrapper.useWrappedStore(pageProps)
   return (
     <Provider store={store}>
       <ApolloProvider client={client}>
-        <Component {...pageProps} />
+        <DefaultLayout>
+          <Component {...pageProps} />
+        </DefaultLayout>
       </ApolloProvider>
     </Provider>
   )
